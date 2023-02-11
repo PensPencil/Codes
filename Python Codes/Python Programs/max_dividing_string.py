a474add465e5b70231b846a50c9754433483b658
PensPencil <92708776+PensPencil@users.noreply.github.com>
2023-02-11 22:42:21 +0530

    Program completed.

--- Python Codes/Python Programs/max_dividing_string.py
+++ Python Codes/Python Programs/max_dividing_string.py
@@ -1,13 +1,23 @@
-str1 = input("Enter first string :")
-str2 = input("Enter second string :")
-ans= ""
-if (len(str1) <= len(str2)) and (str1 in str2):
-    for i in str1:
-        if i not in ans:
-            ans = ans + i
+def emptylist(str1,str2):
+    for i in str1.split(str2):
+        if i != '':
+            return 0
+    return 1
+
+str1 = input("Enter the first string :")
+str2 = input("Enter the second string :")
+ans = ""
+finalans = ""
+if (len(str1) <= len(str2)):
+    minstr = str1
+    maxstr = str2
 else:
-    if str2 in str1:
-        for i in str2:
-            if i not in ans:
-                ans = ans + i
-print(ans)
+    minstr = str2
+    maxstr = str1
+for i in minstr:
+    ans = ans + i
+    if emptylist(maxstr,ans) == 1 and emptylist(minstr,ans) == 1:
+        finalans = ans
+if finalans == '':
+    print("No string which divides both the string.")
+print(finalans)
